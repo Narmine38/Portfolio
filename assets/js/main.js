c72eb82cacace2bda63993ebd485c92f1f42396d
narmine38 <cedric.atzori@le-campus-numerique.fr>
2022-10-14 16:06:23 +0200

    debug skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Personal - v4.9.1
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
@@ -121,7 +116,7 @@
         header.classList.add('header-top')
 
         navlinks.forEach((item) => {
-          if (item.getAttribute('href') == window.location.hash) {
+          if (item.getAttribute('href') === window.location.hash) {
             item.classList.add('active')
           } else {
             item.classList.remove('active')
@@ -140,6 +135,19 @@
   /**
    * Skills animation
    */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
 
   /**
    * Initiate Pure Counter 
